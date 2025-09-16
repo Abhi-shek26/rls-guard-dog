181499bc380c9f8f6fc8321f2520e62ddd044998
Abhi-shek26 <ak4523827@gmail.com>
2025-09-16 19:04:45 +0530

    deployement issue finally resolved

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,30 +1,42 @@
-import { forwardRef } from 'react';
+import { forwardRef, ReactNode } from 'react';
 
-type ButtonAsButton = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+type ButtonAsButton = {
   as?: 'button';
-};
-type ButtonAsAnchor = React.AnchorHTMLAttributes<HTMLAnchorElement> & {
+  children: ReactNode;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+type ButtonAsAnchor = {
   as: 'a';
-};
+  children: ReactNode;
+} & React.AnchorHTMLAttributes<HTMLAnchorElement>;
 
 type ButtonProps = ButtonAsButton | ButtonAsAnchor;
 
 const Button = forwardRef<HTMLButtonElement | HTMLAnchorElement, ButtonProps>(
-  ({ className = '', children, as = 'button', ...props }, ref) => {
-    const Component = as === 'a' ? 'a' : 'button';
+  ({ as = 'button', children, className = '', ...props }, ref) => {
+    if (as === 'a') {
+      return (
+        <a
+          ref={ref as React.Ref<HTMLAnchorElement>}
+          className={`w-full inline-block text-center px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed ${className}`}
+          {...(props as React.AnchorHTMLAttributes<HTMLAnchorElement>)}
+        >
+          {children}
+        </a>
+      );
+    }
 
     return (
-      <Component
-        ref={ref as any}
+      <button
+        ref={ref as React.Ref<HTMLButtonElement>}
         className={`w-full inline-block text-center px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed ${className}`}
-        {...props}
+        {...(props as React.ButtonHTMLAttributes<HTMLButtonElement>)}
       >
         {children}
-      </Component>
+      </button>
     );
   }
 );
 
 Button.displayName = 'Button';
-
 export default Button;
